124c260bdfc3728b8b8ef051bd867f76e6406f39
Justin Gawrilow <justin.gawrilow@gmail.com>
2018-05-16 16:45:08 +0000

    first

---
+++ run.sh
@@ -0,0 +1,1 @@
+spark-submit --master yarn-client --class gov.darpa.hive.Driver target/scala-2.10/hive-v0_2.10-1.0.jar
--- src/main/scala/gov/darpa/hive/Example.scala
+++ src/main/scala/gov/darpa/hive/Example.scala
@@ -1,19 +1,19 @@
+package gov.darpa.hive
+
 import org.apache.spark._
 import org.apache.spark.graphx._
 import org.apache.spark.rdd.RDD
 import scala.collection.mutable.HashMap
 import scala.collection.mutable.Set
+import org.apache.spark.graphx.util.GraphGenerators
+
 
 /*
-maxFlow function
-
-Arguments: sourceId (type: VertexId)
-           targetId (type: VertexId)
-           graph (type: Graph[Long,Int])
-Returns: the maximum flows as RDD[(VertexId,VertexId),Int]
-
-Note that the maxFlow function is defined below the shortestPath function
+Example use of maxFlow function
+(note that maxFlow function needs to be imported in order to run this)
  */
+
+object Driver {
 
 def shortestPath(sourceId: VertexId, targetId: VertexId, graph: Graph[Long, Int]): (Set[(VertexId, VertexId)], Int) = {
   val test: VertexId = sourceId // default vertex id
@@ -79,7 +79,7 @@ def shortestPath(sourceId: VertexId, targetId: VertexId, graph: Graph[Long, Int]
 }
 
 
-def maxFlow ( sourceId: VertexId, targetId: VertexId, graph: Graph[Long,Int] ) : RDD[((VertexId,VertexId),Int)] = {
+def maxFlow ( sc: SparkContext, sourceId: VertexId, targetId: VertexId, graph: Graph[Long,Int] ) : RDD[((VertexId,VertexId),Int)] = {
 
   val edges = graph.edges
   val vertices = graph.vertices
@@ -129,4 +129,72 @@ def maxFlow ( sourceId: VertexId, targetId: VertexId, graph: Graph[Long,Int] ) :
   }
 
   return flows
+}
+
+def main(args: Array[String]) {
+// Running maxFlow on a few random graphs
+val sc: SparkContext = new SparkContext()
+
+for (i <- 1 to 3 )
+{
+  var n: Int = 10*i // Number of vertices
+
+  // Create random graph with random source and target
+  var graph = GraphGenerators.logNormalGraph(sc, n, 5)
+  val r = scala.util.Random
+  val sourceId: VertexId = r.nextInt(n) // The source
+  val targetId: VertexId = r.nextInt(n) // The target
+
+  // Calculate Max Flow
+  val t0 = System.nanoTime()
+  val flows = maxFlow(sc, sourceId, targetId, graph)
+  val t1 = System.nanoTime()
+
+  // Print information
+  val emanating = flows.filter(e => e._1._1 == sourceId).map(e => (e._1._1,e._2)).reduceByKey(_ + _).collect
+  println("Number of Edges: ")
+  println(flows.count)
+  println("Max Flow: ")
+  println(emanating(0)._2)
+  println("Time: ")
+  println((t1 - t0)/1e9)
+}
+
+
+
+/*
+
+//To use a graph imported from a txt file, uncomment this section
+
+// Import vertices
+val vertices = sc.textFile("data/toy-vertices-big.txt").
+  flatMap(line => line.split(" ")).
+  map(l => (l.toLong, 0.toLong)) // Vertex needs a property
+
+// Import Edges
+val edges = sc.textFile("data/toy-edges-big.txt").
+  map(line => line.split(" ")).
+  map(e => ((e(0).toLong, e(1).toLong), e(2).toInt)).
+  reduceByKey(_ + _).
+  map(e => Edge(e._1._1, e._1._2, e._2))
+
+// Create Graph
+val g: Graph[Long,Int] = Graph(vertices, edges)
+
+val s: VertexId = 42 // The source
+val t: VertexId = 73 // The target
+
+val t0 = System.nanoTime()
+val flows = maxFlow(s, t, g)
+val t1 = System.nanoTime()
+
+println("Time: ")
+println((t1 - t0)/1e9)
+
+val emanating = flows.filter(e => e._1._1 == s).map(e => (e._1._1,e._2)).reduceByKey(_ + _).collect
+println("Max Flow: ")
+println(emanating(0)._2)
+
+*/
+}
 }
